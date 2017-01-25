684481ff6e06395a2d59f284aa663db099dae052
JhonnaGarcia <jhonnatangarcia1@hotmail.com>
2017-01-24 21:35:35 -0600

    Ordenamiento Del Array

--- Ejercicio.java
+++ Ejercicio.java
@@ -28,7 +28,42 @@ public class Ejercicio{
 
                 System.out.println("Posicion: " + i + " Valor: " + array[i]);
 
-           }        
+           }
+
+           //Ordenar El Array
+
+           int temp;
+
+           for(int j = 0; j < array.length - 1; j++){
+
+               for (int i = 0; i < array.length - 1; i++){
+
+                    if (array[i] > array[i + 1]) {
+
+                        temp = array[i];
+
+                        array[i] = array[i + 1];
+
+                        array[i + 1] = temp;
+
+
+                    }
+
+               }
+
+
+           }
+
+           
+            //Impresion Array Ordenado
+
+           System.out.println("Impresion Del Array Ordenado: \n");
+
+           for(int i = 0; i < array.length; i++){
+
+                   System.out.println("Posicion: " + i + " Valor: " + array[i]);
+
+               }        
 
         }
 
